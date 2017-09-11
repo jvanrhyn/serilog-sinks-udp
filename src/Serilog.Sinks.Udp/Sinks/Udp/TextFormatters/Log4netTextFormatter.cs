4014c3c07a5ba9c3545e7c3b24751e3a7ce97ab3
Johan van Rhyn <johan@jvanrhyn.co.za>
2017-09-11 11:15:45 +0200

    Added log4net formatter that includes more properties.

---
+++ src/Serilog.Sinks.Udp/Sinks/Udp/TextFormatters/Log4netTextFormatter.cs
@@ -0,0 +1,173 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using System.IO;
+
+namespace Serilog.Sinks.Udp.TextFormatters
+{
+    /// <summary>
+    /// Text formatter serializing log events into log4net complaint XML.
+    /// </summary>
+    public class Log4netTextFormatter : ITextFormatter
+    {
+        private static readonly string SourceContextPropertyName = "SourceContext";
+        private static readonly string ThreadIdPropertyName = "ThreadId";
+        private static readonly string MachineNamePropertyName = "MachineName";
+        private static readonly string UserNamePropertyName = "EnvironmentUserName";
+        private static readonly string MethodPropertyName = "Method";
+        private static readonly string ProcessNamePropertyName = "ProcessName";
+
+        /// <summary>
+        /// Format the log event into the output.
+        /// </summary>
+        /// <param name="logEvent">The event to format.</param>
+        /// <param name="output">The output.</param>
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            output.Write("<log4net:event");
+
+            WriteLogger(logEvent, output);
+            WriteEventTime(logEvent, output);
+            WriteLevel(logEvent, output);
+            WriteThread(logEvent, output);
+            WriteUserName(logEvent, output);
+            WriteProcessName(logEvent, output);
+            output.Write(">");
+
+
+            output.Write("<log4net:locationInfo");
+            WriteLocationInfoClass(logEvent, output);
+            WriteLocationInfoMethod(logEvent, output);
+            output.Write("/>");
+
+            output.Write("<log4net:properties>");
+            WriteHostName(logEvent, output);
+            output.Write("</log4net:properties>");
+
+            WriteMessage(logEvent, output);
+            WriteException(logEvent, output);
+
+            output.Write("</log4net:event>");
+        }
+
+        private static void WriteProcessName(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(ProcessNamePropertyName, out LogEventPropertyValue processName))
+            {
+                output.Write($" domain=\"{((ScalarValue)processName).Value}\"");
+            }
+        }
+
+        private static void WriteLocationInfoClass(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(SourceContextPropertyName, out LogEventPropertyValue sourceContext))
+            {
+                output.Write($" class=\"{((ScalarValue)sourceContext).Value}\"");
+            }
+        }
+
+        private static void WriteLocationInfoMethod(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(MethodPropertyName, out LogEventPropertyValue methodName))
+            {
+                output.Write($" method=\"{((ScalarValue)methodName).Value}\"");
+            }
+        }
+
+        private static void WriteLogger(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(SourceContextPropertyName, out LogEventPropertyValue sourceContext))
+            {
+                output.Write($" logger=\"{((ScalarValue)sourceContext).Value}\"");
+            }
+        }
+
+        private static void WriteUserName(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(UserNamePropertyName, out LogEventPropertyValue userName))
+            {
+                output.Write($" username=\"{((ScalarValue)userName).Value}\"");
+            }
+        }
+
+        private static void WriteHostName(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(MachineNamePropertyName, out LogEventPropertyValue machineName))
+            {
+                output.Write($" <log4net:data name=\"log4net:HostName\" value=\"{((ScalarValue)machineName).Value}\"></log4net:data>");
+            }
+        }
+
+        private static void WriteEventTime(LogEvent logEvent, TextWriter output)
+        {
+            var eventTime = logEvent.Timestamp;
+            output.Write($" timestamp=\"{eventTime.ToString("yyyy-MM-ddTHH:mm:ss.fffzzz")}\"");
+        }
+
+        private static void WriteLevel(LogEvent logEvent, TextWriter output)
+        {
+            string level;
+
+            switch (logEvent.Level)
+            {
+                case LogEventLevel.Fatal:
+                    level = "FATAL";
+                    break;
+
+                case LogEventLevel.Error:
+                    level = "ERROR";
+                    break;
+
+                case LogEventLevel.Warning:
+                    level = "WARN";
+                    break;
+
+                case LogEventLevel.Information:
+                    level = "INFO";
+                    break;
+
+                case LogEventLevel.Debug:
+                    level = "DEBUG";
+                    break;
+
+                default:
+                    level = "TRACE";
+                    break;
+            }
+
+            output.Write($" level=\"{level}\"");
+        }
+
+        private static void WriteThread(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(ThreadIdPropertyName, out LogEventPropertyValue threadId))
+            {
+                output.Write($" thread=\"{((ScalarValue)threadId).Value}\"");
+            }
+        }
+
+        private static void WriteContext(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Properties.TryGetValue(SourceContextPropertyName, out LogEventPropertyValue sourceContext))
+            {
+                output.Write($" class=\"{sourceContext}\"");
+            }
+        }
+
+        private static void WriteMessage(LogEvent logEvent, TextWriter output)
+        {
+            output.Write("<log4net:message>");
+            logEvent.RenderMessage(output);
+            output.Write("</log4net:message>");
+        }
+
+        private static void WriteException(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent.Exception == null)
+            {
+                return;
+            }
+
+            output.Write($"<log4net:throwable>{logEvent.Exception}</log4net:throwable>");
+        }
+    }
+}
